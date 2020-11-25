a579e14cc795e1fee9f815dfc4735c366bbbab03
evndroo <evandro.slipjr@gmail.com>
2020-11-24 23:29:29 -0300

    adicionando variáveis com valores reutilizáveis e definindo componente estilisado de botão.

--- src/Components/Cabecalho/index.jsx
+++ src/Components/Cabecalho/index.jsx
@@ -1,19 +1,47 @@
 import React from "react";
 import bank_logo from "../../assets/images/bank_logo.svg";
+import styled from "styled-components";
+import { corPrimaria } from "../UI/variables"
+
+const StyledHeader = styled.header`
+  background-color: ${corPrimaria};
+  display: flex;
+  justify-content: space-between;
+  padding: 0 15vw;
+  height: 10vh;
+  align-items: center;
+`;
+
+const Logo = styled.img`
+  height: 50px;
+  width: 50px;
+`
+
+const BtnCabecalho = styled.a`
+  text-align: center;
+  border-radius: 3px;
+  padding: 5px 20px;
+  margin: 0 10px;
+  font-weight: 600;
+  border: 2px solid white;
+
+  background: ${(props)=>props.primary ? "white" : corPrimaria};
+  color: ${(props)=>props.primary ? "#41d3be" : "white"};
+`
 
 const Cabecalho = () => {
   return (
-    <div className="cabecalho">
-      <img className="imagem-logo" src={bank_logo} alt="Logo Smart Bank" />
-      <div>
-        <a className="btn-secundario" href="https://google.com">
+    <StyledHeader>
+      <Logo src={bank_logo} alt="Logo Smart Bank" />
+      <nav>
+        <BtnCabecalho primary href="https://google.com">
           Ajuda
-        </a>
-        <a className="btn-primario" href="https://google.com">
+        </BtnCabecalho>
+        <BtnCabecalho href="https://google.com">
           Sair
-        </a>
-      </div>
-    </div>
+        </BtnCabecalho>
+      </nav>
+    </StyledHeader>
   );
 };
 
